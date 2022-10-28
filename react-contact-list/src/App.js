376769f83f0854402d23d8e7910b8f9707fd0a1b
TheodorAmpas <ampastheodwros@gmail.com>
2022-10-28 22:27:21 +0300

    Creating contact card component

--- react-contact-list/src/App.js
+++ react-contact-list/src/App.js
@@ -1,8 +1,15 @@
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="contact-card">
+      <img src="https://via.placeholder.com/150" alt='profile'></img>
+      <div className="user-details">
+        <p>Name: Jenny Han</p>
+        <p>Email: Jenny@gmail.com</p>
+        <p>Age: 25</p>
+      </div>
     </div>
   );
 }
