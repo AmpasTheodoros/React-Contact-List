21668e99e13d943531e39155a527b180bee833cf
TheodorAmpas <ampastheodwros@gmail.com>
2022-10-30 23:52:52 +0200

    ContactCard js

---
+++ react-contact-list/src/ContactCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const ContactCard = () => {
+    return (
+        <div className="contact-card">
+            <img src="https://via.placeholder.com/150" alt='profile'></img>
+            <div className="user-details">
+                <p>Name: Jenny Han</p>
+                <p>Email: Jenny@gmail.com</p>
+                <p>Age: 25</p>
+            </div>
+        </div>
+    )
+}
